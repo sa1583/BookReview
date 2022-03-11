3c74e9d66e5865755a10d580f38adf4d73062c70
sa1583 <sa1583@naver.com>
2022-03-11 16:42:10 +0900

    add showing book's image using glide

--- app/src/main/java/fastcampus/aop/part2/bookreview/adapter/BookAdapter.kt
+++ app/src/main/java/fastcampus/aop/part2/bookreview/adapter/BookAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import fastcampus.aop.part2.bookreview.databinding.ItemBookBinding
 import fastcampus.aop.part2.bookreview.model.Book
 
@@ -12,6 +13,11 @@ class BookAdapter: ListAdapter<Book, BookAdapter.BookItemViewHolder>(diffUtil) {
     inner class BookItemViewHolder(private val binding: ItemBookBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(bookModel: Book) {
             binding.titleTextView.text = bookModel.title
+            binding.descriptionTextView.text = bookModel.description
+            Glide
+                .with(binding.coverImageView.context)
+                .load(bookModel.image)
+                .into(binding.coverImageView)
         }
     }
 
